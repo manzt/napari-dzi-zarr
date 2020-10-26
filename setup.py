43f691dcf3b0b673c464484dfc63002ee372cde4
Trevor Manz <trevor.j.manz@gmail.com>
2020-10-26 16:39:06 -0400

    Add Framework :: napari trove classifier to setup (#4)

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ from setuptools import setup, find_packages
 
 def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
-    return codecs.open(file_path, encoding='utf-8').read()
+    return codecs.open(file_path, encoding="utf-8").read()
 
 
 # Add your dependencies in requirements.txt
 # Note: you can add test-specific requirements in tox.ini
 requirements = []
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     for line in f:
         stripped = line.split("#")[0].strip()
         if len(stripped) > 0:
@@ -25,36 +25,37 @@ with open('requirements.txt') as f:
 use_scm = {"write_to": "napari_dzi_zarr/_version.py"}
 
 setup(
-    name='napari-dzi-zarr',
-    author='Trevor Manz',
-    author_email='trevor.j.manz@gmail.com',
-    license='BSD-3',
-    url='https://github.com/manzt/napari-dzi-zarr',
-    description='An experimental plugin for viewing Deep Zoom Images (DZI) with napari and zarr.',
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
+    name="napari-dzi-zarr",
+    author="Trevor Manz",
+    author_email="trevor.j.manz@gmail.com",
+    license="BSD-3",
+    url="https://github.com/manzt/napari-dzi-zarr",
+    description="An experimental plugin for viewing Deep Zoom Images (DZI) with napari and zarr.",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=requirements,
     use_scm_version=use_scm,
-    setup_requires=['setuptools_scm'],
+    setup_requires=["setuptools_scm"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Testing',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: BSD License',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
+        "Framework :: napari",
     ],
     entry_points={
-        'napari.plugin': [
-            'dzi-zarr = napari_dzi_zarr',
+        "napari.plugin": [
+            "dzi-zarr = napari_dzi_zarr",
         ],
     },
 )
